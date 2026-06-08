21d0323638ac4c374f015b0935f84d178cd2e5da
Yassine Chandid ☁️ <yasssinechandid@gmail.com>
2026-06-08 19:27:57 +0100

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-16ad23c7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.t01u6ip2jho"
+    "revision": "0.kplnqvmsha"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
